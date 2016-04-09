bb27fb6bec5c4a623b6e356b12713c8156627d81
anon <anon@anon>
2016-04-09 12:41:39 -0700

    Now use a conditional.

--- mine.c
+++ mine.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 struct character {
-  uint8_t seen;
+  uint_fast8_t seen;
   struct character *earlier;
 };
 
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
   struct character *character;
 
   for(i = 0; i < sizeof(characters) / sizeof(characters[0]); ++i) {
-    characters[i].seen = 0xff;
+    characters[i].seen = 0;
   }
 
   for(;;) {
@@ -37,23 +37,21 @@ int main(int argc, char *argv[]) {
 
       // If this is the first time we've seen this character, link it 
       // into the order we've first seen them.
-      if(character->seen == 0xff) {
+      if(!character->seen) {
         character->earlier = latest;
         latest = character;
       }
 
-      // Count the times we've seen this value as the number of 0 bits 
-      // left.  Truncates at 8 but does not wrap like adding would (you 
-      // faggots, consider your edge cases) without requiring a 
-      // conditional.
-      character->seen <<= 1;
+      // Count the times we've seen this value and truncate at 2 to 
+      // prevent wrapping.
+      if(character->seen < 2) ++character->seen;
     }
   }
 
   // Walk the list of characters used and choose the earliest that was 
   // seen only once.
   for(character = latest; character; character = character->earlier) {
-    if(character->seen == 0xfe) output[0] = (char) (character - characters);
+    if(character->seen == 1) output[0] = (char) (character - characters);
   }
 
   (void) puts(output);
