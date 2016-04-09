caecdadb6f93668acb092f895ba15fabd85e5822
anon <anon@anon>
2016-04-09 02:34:32 -0700

    Solution for this thread:
    ▶Bi-Millennial /tech/ coding challenge Anonymous  04/08/16 (Fri) 13:04:06 No.560848>>560897 [Watch Thread]
    Write a program which finds the first non-repeated character in a string.
    Example input
    
    yellow
    tooth
    Example output
    
    y
    h
    Write this in any language you want. Solutions provided by women™ are prefered.
    >inb4 do your own homework faggot

--- shit.c
+++ shit.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdio.h>
+#include <memory.h>
 
 struct character {
   uint8_t seen;
@@ -11,7 +12,7 @@ struct character {
 
 int main(int argc, char *argv[]) {
   struct character characters[256];
-  char output = 0;
+  char output[] = {0, 0};
   unsigned char buffer[2 << 12];
   size_t i;
   ssize_t result;
@@ -29,9 +30,6 @@ int main(int argc, char *argv[]) {
     } while(result < 0 && errno == EINTR);
     if(result < 0) return -1;
 
-    // Fuck checking for newlines like an undergrad and having to deal 
-    // with different line endings, we'll watch for EOF and save a few 
-    // cycles.
     if(!result) break;
 
     // Process the buffer.
@@ -56,13 +54,10 @@ int main(int argc, char *argv[]) {
   // Walk the list of characters used and choose the earliest that was 
   // seen only once.
   for(character = latest; character; character = character->earlier) {
-    if(character->seen == 0xfe) output = (char) (character - characters);
+    if(character->seen == 0xfe) output[0] = (char) (character - characters);
   }
 
-  // Since OP left the 'if no such characters exist' case as UNDEFINED 
-  // BEHAVIOR, I choose to write a NULL.  Also, fuck newlines.
-  (void) putchar(output);
-  (void) putchar('\n');
+  (void) puts(output);
 
   return 0;
 }
